a0f6f5b0baa9eadc033c3c066e839f3a0c659c0f
Fredrik Schalling <schalli@kth.se>
2018-11-05 12:34:16 +0100

    Added possibility to run with samples

--- ball_trajectory/ball_trajectory_calculater.m
+++ ball_trajectory/ball_trajectory_calculater.m
@@ -1,43 +1,72 @@
 close all
-rosshutdown
-rosinit
+clc
 
-rostopic list
+DEV_ENVIRONMENT = 1;
+DEV_SAMPLE = 1;
 
-rostopic info /object_update
+if DEV_ENVIRONMENT
+    disp("RUNNING IN DEVELOPMENT ENVIRONMENT")
+    disp("............")
+    load(strcat('samples/sample', int2str(DEV_SAMPLE), '.mat'))
+else
+    rosshutdown
+    rosinit
+    rostopic list
 
-ou = rossubscriber('/object_update')
+    rostopic info /object_update
 
-x_points = []
-y_points = []
-z_points = []
-
-%Check to see if ball has been thrown in the air
-data = receive(ou, 10)
-while(data.Objects.Z/10 < 40 )
-    data = receive(ou, 10)
+    ou = rossubscriber('/object_update')
 end
 
-for i = 1:40
-    data = receive(ou, 10)
-    x_beforelast = 0;
-    y_beforelast = 0;
-    z_beforelast = 0;
-    
-    if(i > 1)
-        x_beforelast = x_points(end);
-        y_beforelast = y_points(end);
-        z_beforelast = z_points(end);
+x_points = [];
+y_points = [];
+z_points = [];
+
+%%
+
+%Check to see if ball has been thrown in the air
+if DEV_ENVIRONMENT
+    i = 1;
+    while points(i).Objects.X < 400
+        i = i + 1;
     end
+    data_array = i:i+50;
+else
+    data = receive(ou, 10);
+    while(data.Objects.Z/10 < 40)
+        data = receive(ou, 10)
+    end
+    data_array = 1:50
+end
+
+for i = data_array
+    if DEV_ENVIRONMENT
+        data = points(i);
+    else
+        data = receive(ou, 10);
+    end
+    
+%     x_beforelast = 0;
+%     y_beforelast = 0;
+%     z_beforelast = 0;
+%     
+%     if(i > 1)
+%         x_beforelast = x_points(end);
+%         y_beforelast = y_points(end);
+%         z_beforelast = z_points(end);
+%     end
     x_points = [x_points data.Objects.X/10];
     y_points = [y_points data.Objects.Y/10];
     z_points = [z_points data.Objects.Z/10];
     
-    if(z_points(end) - z_beforelast < 0)
-        xz_max = x_beforelast;
-        yz_max = y_beforelast;
-        break;
+    if length(z_points)>1 && (z_points(end) - z_points(end-1)) < 0
+        break
     end
+%     if(z_points(end) - z_beforelast < 0)
+%         xz_max = x_beforelast;
+%         yz_max = y_beforelast;
+%         break;
+%     end
     
 end
 %% Fit curve trajectory
@@ -47,15 +76,15 @@ fit_xz = polyfit(x_points', z_points', 2);
 fit_yz = polyfit(y_points', z_points', 2);
 
 %Step 2: Get the points that go through the curve created in the previous step
-[polyfit_xz, polyfit_yz] = Fit_points(z_points, fit_xz, fit_yz ) 
+%[polyfit_xz, polyfit_yz] = Fit_points(z_points, fit_xz, fit_yz ) 
 
 %Step 3: Reflect those points about a point z 
-[reflection_x, reflection_y, reflection_z] = Mirror(polyfit_xz, polyfit_yz, z_points, xz_max, yz_max);
+%[reflection_x, reflection_y, reflection_z] = Mirror(polyfit_xz, polyfit_yz, z_points, xz_max, yz_max);
 %[reflection_x, reflection_y, reflection_z] = Mirror(x_points, y_points, z_points, xz_max, yz_max);
 
-x_points = [x_points reflection_x];
-y_points = [y_points reflection_y];
-z_points = [z_points reflection_z];
+% x_points = [x_points reflection_x];
+% y_points = [y_points reflection_y];
+% z_points = [z_points reflection_z];
 
 
 %fit_xz_reflection = polyfit(reflection_x', reflection_z', 2);
@@ -76,13 +105,13 @@ y1 = polyval(fit_xz, x1);
 plot(x1, y1)
 
 %Reflection curves
-plot(reflection_x, reflection_z, '+b')
-hold on
-grid on
+% plot(reflection_x, reflection_z, '+b')
+% hold on
+% grid on
 
-x1 = linspace(reflection_x(1), reflection_x(end), 1000);
-y1 = polyval(fit_xz_reflection, x1);
-plot(x1, y1)
+% x1 = linspace(reflection_x(1), reflection_x(end), 1000);
+% y1 = polyval(fit_xz_reflection, x1);
+% plot(x1, y1)
 
 
 % YZ
