a2f3b5f4b7bfba82f84257918e5dd8064f1245b3
Fredrik Schalling <schalli@kth.se>
2018-11-05 15:09:14 +0100

    Now calculates intersection points

--- ball_trajectory/Mirror.m
+++ ball_trajectory/Mirror.m
@@ -1,17 +1,12 @@
 function [reflection_x, reflection_y, reflection_z] = Mirror(x_points, y_points, z_points, z_plane_x, z_plane_y)
-reflection_x = []
-reflection_y = []
-reflection_z = []
+    reflection_x = [];
+    reflection_y = [];
+    reflection_z = [];
 
-for i = 1:numel(x_points)
-    
-   reflection_x = [reflection_x z_plane_x*2-x_points(i)]
-   reflection_y = [reflection_y z_plane_y*2-y_points(i)]
-   reflection_z = [reflection_z z_points(i)]
-    
-end
+    for i = numel(x_points):-1:1
+       reflection_x = [reflection_x z_plane_x*2-x_points(i)];
+       reflection_y = [reflection_y z_plane_y*2-y_points(i)];
+       reflection_z = [reflection_z z_points(i)];
 
-
-
-
+    end
 end
--- ball_trajectory/ball_trajectory_calculater.m
+++ ball_trajectory/ball_trajectory_calculater.m
@@ -2,13 +2,18 @@ close all
 clc
 
 DEV_ENVIRONMENT = 1;
-DEV_SAMPLE = 1;
+DEV_SAMPLE = 2;
 
 if DEV_ENVIRONMENT
-    disp("RUNNING IN DEVELOPMENT ENVIRONMENT")
+    disp("RUNNING IN LOCAL ENVIRONMENT")
     disp("............")
     load(strcat('samples/sample', int2str(DEV_SAMPLE), '.mat'))
+    sample_x = x_points;
+    sample_y = y_points;
+    sample_z = z_points;
 else
+    disp("RUNNING IN ROS ENVIRONMENT")
+    disp("............")
     rosshutdown
     rosinit
     rostopic list
@@ -27,13 +32,13 @@ z_points = [];
 %Check to see if ball has been thrown in the air
 if DEV_ENVIRONMENT
     i = 1;
-    while points(i).Objects.X < 400
+    while points(i).Objects.Z < 400
         i = i + 1;
     end
     data_array = i:i+50;
 else
     data = receive(ou, 10);
-    while(data.Objects.Z/10 < 40)
+    while(data.Objects.Z < 400)
         data = receive(ou, 10)
     end
     data_array = 1:50
@@ -45,95 +50,71 @@ for i = data_array
     else
         data = receive(ou, 10);
     end
-    
-%     x_beforelast = 0;
-%     y_beforelast = 0;
-%     z_beforelast = 0;
-%     
-%     if(i > 1)
-%         x_beforelast = x_points(end);
-%         y_beforelast = y_points(end);
-%         z_beforelast = z_points(end);
-%     end
-    x_points = [x_points data.Objects.X/10];
-    y_points = [y_points data.Objects.Y/10];
-    z_points = [z_points data.Objects.Z/10];
-    
-    if length(z_points)>1 && (z_points(end) - z_points(end-1)) < 0
+
+    if length(z_points)>1 && (data.Objects.Z - z_points(end)) < 0
+        xz_max = x_points(end);
+        yz_max = y_points(end);
         break
+    else
+        x_points = [x_points data.Objects.X];
+        y_points = [y_points data.Objects.Y];
+        z_points = [z_points data.Objects.Z];
     end
-%     if(z_points(end) - z_beforelast < 0)
-%         xz_max = x_beforelast;
-%         yz_max = y_beforelast;
-%         break;
-%     end
     
 end
 %% Fit curve trajectory
 
 %Step 1: Fit curve through original data points
-fit_xz = polyfit(x_points', z_points', 2);
-fit_yz = polyfit(y_points', z_points', 2);
+% fit_xz = polyfit(x_points', z_points', 2);
+% fit_yz = polyfit(y_points', z_points', 2);
 
 %Step 2: Get the points that go through the curve created in the previous step
 %[polyfit_xz, polyfit_yz] = Fit_points(z_points, fit_xz, fit_yz ) 
 
-%Step 3: Reflect those points about a point z 
-%[reflection_x, reflection_y, reflection_z] = Mirror(polyfit_xz, polyfit_yz, z_points, xz_max, yz_max);
-%[reflection_x, reflection_y, reflection_z] = Mirror(x_points, y_points, z_points, xz_max, yz_max);
+%Step 3: Reflect those points about a point z
+[reflection_x, reflection_y, reflection_z] = Mirror(x_points, y_points, z_points, xz_max, yz_max);
 
-% x_points = [x_points reflection_x];
-% y_points = [y_points reflection_y];
-% z_points = [z_points reflection_z];
+x_points = [x_points reflection_x];
+y_points = [y_points reflection_y];
+z_points = [z_points reflection_z];
 
 
-%fit_xz_reflection = polyfit(reflection_x', reflection_z', 2);
-%fit_yz_reflection = polyfit(reflection_y', reflection_z', 2);
+% fit_xz_reflection = polyfit(reflection_x', reflection_z', 2);
+% fit_yz_reflection = polyfit(reflection_y', reflection_z', 2);
 
 %Step 4: Fit a curve through the reflected points
-fit_xz_reflection = polyfit(x_points', z_points', 2);
-fit_yz_reflection = polyfit(y_points', z_points', 2);
+fit_xz_reflection = polyfit(reflection_x', reflection_z', 2);
+fit_yz_reflection = polyfit(reflection_y', reflection_z', 2);
 
-close all
-plot(x_points, z_points, '+r')
-title('XZ')
+figure
+
+plot(sample_x, sample_z, '+b')
 hold on
 grid on
+%plot(x_points, z_points, '+r')
+title('XZ')
 
-x1 = linspace(x_points(1), x_points(end), 1000);
-y1 = polyval(fit_xz, x1);
+
+x1 = linspace(reflection_x(1), 850, 1000);
+y1 = polyval(fit_xz_reflection, x1);
 plot(x1, y1)
-
-%Reflection curves
-% plot(reflection_x, reflection_z, '+b')
-% hold on
-% grid on
-
-% x1 = linspace(reflection_x(1), reflection_x(end), 1000);
-% y1 = polyval(fit_xz_reflection, x1);
-% plot(x1, y1)
-
 
 % YZ
 figure
-plot(y_points, z_points, '+r')
-title('YZ')
+plot(sample_y, sample_z, '+b')
 hold on
 grid on
+%plot(y_points, z_points, '+r')
+title('YZ')
 
-x1 = linspace(y_points(1), y_points(end), 1000);
-y1 = polyval(fit_yz, x1);
+
+x1 = linspace(reflection_y(1), -600, 1000);
+y1 = polyval(fit_yz_reflection, x1);
 plot(x1, y1)
 
-%roots(fit_xz)
+[x_intersect, y_intersect] = get_intersection(fit_xz_reflection, fit_yz_reflection, 150)
 
 
 
-%%
-%plot(x_points, z_points, '+')
-%hold on
 
-%figure
-%plot(y_points, z_points, '+')
-%hold on
 
