1fe2b8a7194b5a5818a55f96b38dbcf1f4dd771a
Fredrik Schalling <schalli@kth.se>
2018-11-08 16:24:36 +0100

    Formatting

--- arduino_control/low_lwl_controller/low_lwl_controller.ino
+++ arduino_control/low_lwl_controller/low_lwl_controller.ino
@@ -19,7 +19,6 @@ ros::NodeHandle  nh;
 
 //std_msgs::String str_msg;
 std_msgs::Int32 int_msg;
-char hello[13] = "hello world!";
 
 void messageCb( const std_msgs::Empty& toggle_msg){
   digitalWrite(13, HIGH-digitalRead(13));   // blink the led
@@ -32,39 +31,39 @@ void setup() {
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(arduino_printer);
+  
   /*
   //Set up the hardware serial for debugging
   Serial.begin(9600); //for manual debugging, with no ROS
   Serial.flush();*/
   
-  //Set up the software serial
+  // Set up the software serial
   //encoderSerial.begin(9600);
   //encoderSerial.flush();
   
-  //make sure that the software serial pins are input
+  // Make sure that the software serial pins are input
   pinMode(RX_pin, INPUT);
   pinMode(TX_pin, INPUT);
   
-  //Make pin 7 an input
+  // Make pin 7 an input
   pinMode(7, INPUT);
-
-
   pinMode(13, OUTPUT);
   
 }
 
 void loop() {
-  
   // The bit-shifting method
   compactMsg = encoderSerial.parseInt();
   enc_count = compactMsg>>10;
   
   enc_count = compactMsg;
   //encoderSerial.flush();
-  
+
+  // Prepare and send message to ROS
   int_msg.data = enc_count;
   arduino_printer.publish(&int_msg);
   nh.spinOnce();
+  
   delay(10);
 
 }
