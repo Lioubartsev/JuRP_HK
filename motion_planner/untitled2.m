81d86b9585d8eb0a0c3b8e540d81f5e47b143cb6
Fredrik Schalling <schalli@kth.se>
2018-11-26 17:03:56 +0100

    Changes were made to the low level controller of the upper arm. By Dennis and Ahmed.

--- motion_planner/untitled2.m
+++ motion_planner/untitled2.m
@@ -1,11 +1,13 @@
-rosshutdown
-rosinit
-%while(1)
-values = [1 2 3 4 5 6 7 8 9];
-values = rand(10,1);
+%rosshutdown
+%rosinit
+angle = -600;
 
-shoulder_reference = rospublisher('/shoulder_reference', 'std_msgs/Float32MultiArray');
-shoulder_reference_msg = rosmessage(shoulder_reference);
+%while(1)
+
+
+
+upper_reference = rospublisher('/upper_reference', 'std_msgs/Int16');
+upper_reference_msg = rosmessage(upper_reference);
 
 
 %shoulder_reference = rospublisher('/shoulder_reference', 'std_msgs/Int32MultiArray');
@@ -14,7 +16,9 @@ shoulder_reference_msg = rosmessage(shoulder_reference);
 %shoulder_reference = rospublisher('/shoulder_reference', 'std_msgs/Int32');
 %shoulder_reference_msg = rosmessage(shoulder_reference);
 
-shoulder_reference_msg.Data = values;
-send(shoulder_reference, shoulder_reference_msg) 
+upper_reference_msg.Data = angle;
 
+send(upper_reference, upper_reference_msg) 
+
+%pause(1)
 %end
